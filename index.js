3a31d0b9f6861ec1dc82a59ee6123533c7fa04db
RacheSa <rachel.salois@gmail.com>
2019-06-09 20:47:24 -0400

    adds an image

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('ok')
+var fruitsDiv = document.getElementById('fruits');
+var image = document.createElement("img");
+image.src = 'images/berry-1238249_1920.jpg'
+fruitsDiv.appendChild(image);
