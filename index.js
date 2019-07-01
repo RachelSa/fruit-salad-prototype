0fd9684cfdd1e196533f2e6942079695b212df25
RacheSa <rachel.salois@gmail.com>
2019-06-30 20:42:01 -0400

    adds button

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const paths = ['blackberry.png', 'kiwi.png', 'orange.png', 'banana.png']
 const fruitButtonsDiv = document.getElementById('fruit-buttons')
 const fruitDetail = document.getElementById('selected-fruit')
 
-fruitImage = (path, styles = '') => {
-  return `<img src='images/${path}' alt=${path} class=${styles}></img>`
+fruitImage = (path, styles) => {
+  return `<img src='images/${path}' alt=${path} class='${styles}'></img>`
 }
 
 onFruitButtonClick = (path) => {
   fruitDetail.innerHTML = ''
   let div = document.createElement('div')
-  div.innerHTML = fruitImage(path, 'rotating')
-  div.className += 'margin-xxl padding-xxl '
+  div.innerHTML = fruitImage(path, 'rotating med-width')
+  div.className += 'med-width'
   fruitDetail.appendChild(div)
 }
 
 addFruitButtons = (paths) => {
   paths.forEach((path) => {
     let button = document.createElement('button')
-    button.innerHTML = fruitImage(path, 'icon padding-s')
+    button.innerHTML = fruitImage(path, 'icon')
     button.className += 'invisi-button margin-s'
     button.addEventListener('click', () => onFruitButtonClick(path))
     fruitButtonsDiv.appendChild(button)
