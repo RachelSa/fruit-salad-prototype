4bdf80caccef819bff0729d6510fdb1ef4142794
RacheSa <rachel.salois@gmail.com>
2019-07-13 18:49:35 -0400

    next button click through screen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const paths = ['blackberry.png', 'kiwi.png', 'orange.png', 'banana.png']
 const fruitButtonsDiv = document.getElementById('fruit-buttons')
 const fruitDetail = document.getElementById('selected-fruit')
+let selectedFruitPath = ''
 
 fruitImage = (path, styles) => {
   return `<img src='images/${path}' alt=${path} class='${styles}'></img>`
@@ -12,16 +13,40 @@ onFruitButtonClick = (path) => {
   div.innerHTML = fruitImage(path, 'rotating med-width')
   div.className += 'med-width'
   fruitDetail.appendChild(div)
+  selectedFruitPath = path
 }
 
-addFruitButtons = (paths) => {
+createFruitButton = (path) => {
+  let button = document.createElement('button')
+  button.innerHTML = fruitImage(path, 'icon')
+  button.className += 'invisi-button margin-s'
+  button.addEventListener('click', () => onFruitButtonClick(path))
+  fruitButtonsDiv.appendChild(button)
+}
+
+createFruitButtons = (paths) => {
   paths.forEach((path) => {
-    let button = document.createElement('button')
-    button.innerHTML = fruitImage(path, 'icon')
-    button.className += 'invisi-button margin-s'
-    button.addEventListener('click', () => onFruitButtonClick(path))
-    fruitButtonsDiv.appendChild(button)
+    createFruitButton(path)
   })
 }
 
-addFruitButtons(paths)
+onNextButtonClick = (path) => {
+  fruitButtonsDiv.innerHTML = ""
+  if (path === 'banana.png') {
+    fruitDetail.innerHTML = "You picked banana."
+  } else {
+    fruitDetail.innerHTML = "Orange you glad you didn't pick banana?"
+  }
+}
+
+createNextButtonHandler = () => {
+  const nextButton = document.getElementById('next-button')
+  nextButton.addEventListener('click', () => onNextButtonClick(selectedFruitPath))
+}
+
+addFruitSelectorSetup = (paths) => {
+  createFruitButtons(paths)
+  createNextButtonHandler()
+}
+
+addFruitSelectorSetup(paths)
