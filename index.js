70dff62e989dca5ec7426d119cbe778bb9e2ab4a
RacheSa <rachel.salois@gmail.com>
2019-07-21 11:52:24 -0400

    adds back to fruit picker

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+import FruitButton from '/javascript/fruitButton.js'
 const paths = ['blackberry.png', 'kiwi.png', 'orange.png', 'banana.png']
 const fruitButtonsDiv = document.getElementById('fruit-buttons')
 const fruitDetail = document.getElementById('selected-fruit')
 let selectedFruitPath = ''
 
-fruitImage = (path, styles) => {
+const fruitImage = (path, styles) => {
   return `<img src='images/${path}' alt=${path} class='${styles}'></img>`
 }
 
-onFruitButtonClick = (path) => {
+const onFruitButtonClick = (path) => {
   fruitDetail.innerHTML = ''
   let div = document.createElement('div')
   div.innerHTML = fruitImage(path, 'rotating med-width')
@@ -16,21 +17,21 @@ onFruitButtonClick = (path) => {
   selectedFruitPath = path
 }
 
-createFruitButton = (path) => {
-  let button = document.createElement('button')
-  button.innerHTML = fruitImage(path, 'icon')
-  button.className += 'invisi-button margin-s'
+const createFruitButton = (path) => {
+  let button = new FruitButton(path).create()
   button.addEventListener('click', () => onFruitButtonClick(path))
   fruitButtonsDiv.appendChild(button)
 }
 
-createFruitButtons = (paths) => {
+const createFruitButtons = (paths) => {
   paths.forEach((path) => {
     createFruitButton(path)
   })
 }
 
-onNextButtonClick = (path) => {
+const onNextButtonClick = (path) => {
+  toggleNextBackButtons('toBack')
+  createBackButtonHandler()
   fruitButtonsDiv.innerHTML = ""
   if (path === 'banana.png') {
     fruitDetail.innerHTML = "You picked banana."
@@ -39,14 +40,41 @@ onNextButtonClick = (path) => {
   }
 }
 
-createNextButtonHandler = () => {
+const createNextButtonHandler = () => {
   const nextButton = document.getElementById('next-button')
   nextButton.addEventListener('click', () => onNextButtonClick(selectedFruitPath))
 }
 
-addFruitSelectorSetup = (paths) => {
+const onBackButtonClick = () => {
+  fruitDetail.innerHTML = ""
+  addFruitSelectorSetup(paths)
+  toggleNextBackButtons('toNext')
+}
+
+const createBackButtonHandler = () => {
+  const nextButton = document.getElementById('back-button')
+  nextButton.addEventListener('click', onBackButtonClick)
+}
+
+const addFruitSelectorSetup = (paths) => {
   createFruitButtons(paths)
   createNextButtonHandler()
+}
+
+const toggleNextBackButtons = (direction) => {
+  switch (direction){
+    case 'toBack':
+      document.getElementById("next-button").style.visibility = "hidden"
+      document.getElementById("back-button").style.visibility = "visible"
+      break;
+    case 'toNext':
+      document.getElementById("next-button").style.visibility = "visible"
+      document.getElementById("back-button").style.visibility = "hidden"
+      break;
+    default:
+      document.getElementById("next-button").style.visibility = "visible"
+      document.getElementById("back-button").style.visibility = "hidden"
+  }
 }
 
 addFruitSelectorSetup(paths)
